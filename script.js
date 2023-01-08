71382bfbaac76c231ba2ea623bef26af0816feb5
Gwazza23 <weerawarnagayan@gmail.com>
2023-01-08 13:12:12 +0000

    add about section

---
+++ script.js
@@ -0,0 +1,17 @@
+let button = document.getElementById('aboutButton');
+let about = document.querySelector('#about p');
+let closeButton = document.getElementById('closeButton');
+function showAbout(){
+    button.style.display = 'none';
+    about.style.display = 'block';
+    closeButton.style.display = 'block';
+}
+
+function showAboutButton(){
+    button.style.display = 'block';
+    about.style.display = 'none';
+    closeButton.style.display ='none'
+}
+
+button.addEventListener('click',showAbout);
+closeButton.addEventListener('click',showAboutButton);
